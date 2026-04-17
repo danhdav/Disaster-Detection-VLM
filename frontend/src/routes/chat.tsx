50784bb5b63b8fe4c9e7e597a765ba539a255634
Jason Antwi-Appah <hey@jasonaa.me>
2026-04-17 14:43:51 -0500

    fixup

--- frontend/src/routes/chat.tsx
+++ frontend/src/routes/chat.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { ChatSidebar } from "../components/ChatSidebar.tsx";
+import { ChatSidebar } from "../components/ChatSidebar/ChatSidebar";
 
 export const Route = createFileRoute("/chat")({
   component: ChatPage,
